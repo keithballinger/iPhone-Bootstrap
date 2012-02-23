364d465409214685e91deb6ce15db862d75684ab
Keith Ballinger <keithba@gmail.com>
2012-02-22 19:58:02 -0500

    ATMHud support

--- iPhone-Bootstrap/AppDelegate.m
+++ iPhone-Bootstrap/AppDelegate.m
@@ -23,6 +23,9 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    
+    
     return YES;
 }
 
