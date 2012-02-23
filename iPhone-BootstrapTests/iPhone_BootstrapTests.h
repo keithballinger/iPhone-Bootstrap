49736ec319f9a59251aa7835318c970c15c5b795
Keith Ballinger <keithba@gmail.com>
2012-02-22 19:34:48 -0500

    initial project

---
+++ iPhone-BootstrapTests/iPhone_BootstrapTests.h
@@ -0,0 +1,13 @@
+//
+//  iPhone_BootstrapTests.h
+//  iPhone-BootstrapTests
+//
+//  Created by Keith Ballinger on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iPhone_BootstrapTests : SenTestCase
+
+@end
---
+++ iPhone-BootstrapTests/iPhone_BootstrapTests.m
@@ -0,0 +1,32 @@
+//
+//  iPhone_BootstrapTests.m
+//  iPhone-BootstrapTests
+//
+//  Created by Keith Ballinger on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "iPhone_BootstrapTests.h"
+
+@implementation iPhone_BootstrapTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iPhone-BootstrapTests");
+}
+
+@end
